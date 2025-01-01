2a9ab64bb77c4f00a51661d40914e62b8d1c81db
raaman007 <84496211+raaman007@users.noreply.github.com>
2025-01-01 20:27:21 +0530

    Add files via upload

---
+++ email.py
@@ -0,0 +1,5 @@
+email="ilearniexcel@gmail.com"
+print("the position of @ is :",email.find("@"))
+print("printing the last four characters of email :",email[-4:])
+print("Extarct only gmail from the mail :",email[13:18])
+print("Splitting the email into parts: ",email.replace(".","@").split("@"))
